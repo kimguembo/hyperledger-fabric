0f890a1301e1bfb0563cb13600b43ce182395cdd
LeeHyeonHee <ddrtocna@gmail.com>
2021-06-23 18:07:43 +0900

    fixed logic error and business logic

--- chaincode-user/chaincode/usercontract.go
+++ chaincode-user/chaincode/usercontract.go
@@ -105,8 +105,32 @@ func (s *UserContract) UpdateAccount(ctx contractapi.TransactionContextInterface
 	return ctx.GetStub().PutState(id, accountJSON)	
 }
 
+// 은행에서 돈발행 
+func (s *UserContract) UpdateUserAccount(ctx contractapi.TransactionContextInterface, bankID string, id string, balance string) error {
+	account, err := s.ReadAccount(ctx, id)
+	if err != nil {
+		return err
+	}
+	balNum, e := strconv.Atoi(balance)
+	if e != nil {
+		return e
+	}
+
+	newBal := account.Balance + balNum
+	if newBal > MAX_VAL {
+		return fmt.Errorf("Individuals cannot own more than %s in CBDC.", MAX_VAL)
+	}
+	account.Balance = newBal
+	accountJSON, err := json.Marshal(account)
+	if err != nil {
+		return err
+	}
+	
+	return ctx.GetStub().PutState(id, accountJSON)	
+}
+
 // user 끼리의 돈전송 
-func (s *UserContract) TransferBalanceUser(ctx contractapi.TransactionContextInterface, id string, rec string, price int) error {
+func (s *UserContract) TransferBalanceUser(ctx contractapi.TransactionContextInterface, bankID string, id string, rec string, price int) error {
 	sender, err := s.ReadAccount(ctx, id)
 	if err != nil {
 		return err
@@ -125,23 +149,32 @@ func (s *UserContract) TransferBalanceUser(ctx contractapi.TransactionContextInt
 		return fmt.Errorf("Individuals cannot own more than %s in CBDC.", MAX_VAL)
 	}
 	sender.Balance = sBal
-	receiver.Balance = rBal
+	// receiver.Balance = rBal
 
 	senderJSON, sErr := json.Marshal(sender)
 	if sErr != nil {
 		return sErr
 	}
-	receiverJSON, rErr := json.Marshal(receiver)
-	if rErr != nil {
-		return rErr
+	// receiverJSON, rErr := json.Marshal(receiver)
+	// if rErr != nil {
+	// 	return rErr
+	// }
+	params := []string{"AccountExist", bankID}
+	queryArgs := make([][]byte, len(params))
+
+	for i, arg := range params {
+		queryArgs[i] = []byte(arg)
+	}
+	response := ctx.GetStub().InvokeChaincode("regulatorychaincode", queryArgs, "regulatory-channel")
+	if response.Status != 200 {
+		return fmt.Errorf("Failed to query chaincode. Got Error: %s", response.Payload)
 	}
 
-	ctx.GetStub().PutState(id, senderJSON)
-	ctx.GetStub().PutState(rec, receiverJSON)
+	// ctx.GetStub().PutState(rec, receiverJSON)
 
 	//기록 
-	s.TransferHistory(ctx, rec, id, strconv.Itoa(price))
-	return nil	
+	s.TransferHistory(ctx, id, rec, strconv.Itoa(price))
+	return ctx.GetStub().PutState(id, senderJSON)
 }
 
 func (s *UserContract) TransferHistory(ctx contractapi.TransactionContextInterface, rec string, sen string, price string) error {
@@ -185,6 +218,32 @@ func (s *UserContract) ReadTransferHistory(ctx contractapi.TransactionContextInt
 			return nil, err
 		}
 		historys = append(historys, &history)
+	}
+	return historys, nil
+}
+
+
+func (s *UserContract) ReadHistoryUserOnly(ctx contractapi.TransactionContextInterface, userID string) ([]*AccountHistory, error) {
+	historyJSON, err := ctx.GetStub().GetStateByRange("0", "999")
+	if err != nil {
+		return nil, err
+	}
+	defer historyJSON.Close()
+	var historys []*AccountHistory
+	for historyJSON.HasNext() {
+		queryResponse, err := historyJSON.Next()
+
+		if err != nil {
+			return nil, err
+		}
+		var history AccountHistory
+		err = json.Unmarshal(queryResponse.Value, &history)
+		if err != nil {
+			return nil, err
+		}
+		if history.Receiver == userID || history.Sender == userID{
+			historys = append(historys, &history)
+		}
 	}
 	return historys, nil
 }
